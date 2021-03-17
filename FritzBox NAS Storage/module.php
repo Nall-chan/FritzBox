6cf6b7f7a6a53297923d4d3c353e5522ceb783c1
Michael Tröger <mt@neo-ami.de>
2021-03-17 22:40:22 +0100

    WIP

---
+++ FritzBox NAS Storage/module.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+require_once __DIR__ . '/../libs/FritzBoxBase.php';
+class FritzBoxNASStorage extends FritzBoxModulBase
+{
+    protected static $ControlUrlArray = [
+        '/upnp/control/x_storage'
+    ];
+    protected static $EventSubURLArray = [];
+    protected static $ServiceTypeArray = [
+        'urn:dslforum-org:service:X_AVM-DE_Storage:1'
+    ];
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        $this->RegisterPropertyInteger('Index', -1);
+        $this->RegisterPropertyInteger('RefreshInterval', 3600);
+        $this->RegisterTimer('RefreshInfo', 0, 'IPS_RequestAction(' . $this->InstanceID . ',"RefreshInfo",true);');
+    }
+
+    public function Destroy()
+    {
+        //Never delete this line!
+        parent::Destroy();
+    }
+    public function ApplyChanges()
+    {
+        $this->SetTimerInterval('RefreshInfo', 0);
+        parent::ApplyChanges();
+        $this->SetTimerInterval('RefreshInfo', $this->ReadPropertyInteger('RefreshInterval')*1000);
+        if (IPS_GetKernelRunlevel() != KR_READY) {
+            return;
+        }
+        $this->UpdateInfo();
+    }
+    public function RequestAction($Ident, $Value)
+    {
+        if (parent::RequestAction($Ident, $Value)) {
+            return true;
+        }
+        switch ($Ident) {
+            case 'RefreshInfo':
+                return $this->UpdateInfo();
+            case 'SMBEnable':
+                return $this->SetSMBServer($Value);
+            case 'FTPEnable':
+                return $this->SetFTPServer($Value);
+            case 'FTPWANEnable':
+                return $this->SetFTPServerWAN($Value, $this->GetValue('FTPWANSSLOnly'));
+            case 'FTPWANSSLOnly':
+                return $this->SetFTPServerWAN($this->GetValue('FTPWANEnable'), $Value);
+            }
+        trigger_error($this->Translate('Invalid Ident.'), E_USER_NOTICE);
+        return false;
+    }
+    private function UpdateInfo()
+    {
+        $result = $this->GetInfo();
+        if ($result === false) {
+            return false;
+        }
+        $this->setIPSVariable('SMBEnable', 'SMB server', (bool)$result['NewSMBEnable'], VARIABLETYPE_BOOLEAN, '~Switch', true);
+        $this->setIPSVariable('FTPEnable', 'FTP server', (bool)$result['NewFTPEnable'], VARIABLETYPE_BOOLEAN, '~Switch', true);
+        $this->setIPSVariable('FTPStatus', 'FTP server state', (string)$result['NewFTPStatus'], VARIABLETYPE_STRING);
+        $this->setIPSVariable('FTPWANEnable', 'WAN FTP server', (bool)$result['NewFTPWANEnable'], VARIABLETYPE_BOOLEAN, '~Switch', true);
+        $this->setIPSVariable('FTPWANSSLOnly', 'WAN FTP SSL only', (bool)$result['NewFTPWANSSLOnly'], VARIABLETYPE_BOOLEAN, '~Switch', true);
+        $this->setIPSVariable('FTPWANPort', 'WAN FTP Port', (int)$result['NewFTPWANPort'], VARIABLETYPE_INTEGER);
+        return true;
+    }
+    public function GetInfo()
+    {
+        $result = $this->Send(__FUNCTION__);
+        if ($result === false) {
+            return false;
+        }
+        return $result;
+    }
+    public function SetSMBServer(bool $Enable)
+    {
+        $result = $this->Send(__FUNCTION__, [
+            'NewSMBEnable'=> (int)$Enable
+        ]);
+        if ($result === null) {
+            $this->UpdateInfo();
+            return true;
+        }
+        return false;
+    }
+    public function SetFTPServer(bool $Enable)
+    {
+        $result = $this->Send(__FUNCTION__, [
+            'NewFTPEnable'=> (int)$Enable
+        ]);
+        if ($result === null) {
+            $this->UpdateInfo();
+            return true;
+        }
+        return false;
+    }
+    public function SetFTPServerWAN(bool $Enable, bool $SSLOnly)
+    {
+        $result = $this->Send(__FUNCTION__, [
+            'NewFTPWANEnable'=> (int)$Enable,
+            'NewFTPWANSSLOnly'=> (int)$SSLOnly
+        ]);
+        if ($result === null) {
+            $this->UpdateInfo();
+            return true;
+        }
+        return false;
+    }
+}
--- libs/FritzBoxModule.php
+++ libs/FritzBoxModule.php
@@ -21,19 +21,21 @@ class Services
         'urn:dslforum-org:service:WANIPConnection:1'             => ['{9396D756-40EA-46C7-AA06-623B8DCB789B}'=>1], //bei Cabel
         'urn:dslforum-org:service:X_AVM-DE_UPnP:1'               => ['{0F09E36F-BE54-D01D-5AF1-48FF618426AC}'=>0],
         'urn:dslforum-org:service:X_AVM-DE_WebDAVClient:1'       => ['{EA069D88-287E-9F08-DB85-799EBA7A2678}'=>0],
+        'urn:dslforum-org:service:X_AVM-DE_Storage:1'            => ['{14588B6C-6F13-A3C1-0C79-88B6624E1D87}'=>0],
         //TODO
         //prüfen / Fertig bauen
         'urn:schemas-upnp-org:service:WANDSLLinkConfig:1'        => ['{061AEAF5-ADCD-AD9E-9BF9-AD40DF364EB6}'=>0], // Prüfen im Feldtest
         'urn:dslforum-org:service:WANPPPConnection:1'            => ['{9396D756-40EA-46C7-AA06-623B8DCB789B}'=>0], //bei DSL (prüfen im Feldtest)
         //Todo
+        'urn:dslforum-org:service:X_AVM-DE_RemoteAccess:1'       => [],
+        'urn:dslforum-org:service:X_AVM-DE_MyFritz:1'            => [],
         'urn:dslforum-org:service:Layer3Forwarding:1'            => [],
+        'urn:schemas-upnp-org:service:WANIPv6FirewallControl:1'  => [],
         'urn:dslforum-org:service:LANConfigSecurity:1'           => [],
         'urn:dslforum-org:service:ManagementServer:1'            => [],
         'urn:dslforum-org:service:UserInterface:1'               => [],
-        'urn:dslforum-org:service:X_AVM-DE_Storage:1'            => [],
         'urn:dslforum-org:service:X_AVM-DE_Speedtest:1'          => [],
-        'urn:dslforum-org:service:X_AVM-DE_RemoteAccess:1'       => [],
-        'urn:dslforum-org:service:X_AVM-DE_MyFritz:1'            => [],
+        'urn:dslforum-org:service:X_AVM-DE_Filelinks:1'          => [],
         'urn:dslforum-org:service:X_VoIP:1'                      => [],
         'urn:dslforum-org:service:X_AVM-DE_OnTel:1'              => ['{AD0B22A7-C71C-71DD-A40B-B70334C5AB3C}'=>0],
         'urn:dslforum-org:service:X_AVM-DE_Dect:1'               => [],
@@ -41,15 +43,13 @@ class Services
         'urn:dslforum-org:service:X_AVM-DE_AppSetup:1'           => [],
         //'urn:dslforum-org:service:X_AVM-DE_Homeauto:1'           => [],
         //'urn:dslforum-org:service:X_AVM-DE_Homeplug:1'           => [],
-        'urn:dslforum-org:service:X_AVM-DE_Filelinks:1'          => [],
         //'urn:dslforum-org:service:X_AVM-DE_Auth:1'               => [],
         //'urn:dslforum-org:service:LANEthernetInterfaceConfig:1'  => [], //statistik. unnötig?
         //'urn:dslforum-org:service:WANDSLInterfaceConfig:1'       => [], //statistik. unnötig?
         //'urn:dslforum-org:service:WANEthernetLinkConfig:1'       => [], // unnötig?
         //'urn:dslforum-org:service:WANDSLLinkConfig:1'            => [], // unnötig ? DSL Daten?
-        'urn:schemas-upnp-org:service:WANIPv6FirewallControl:1'  => [],
         // Dummy
-        'urn:schemas-any-com:service:Any:1'                      => []
+        //'urn:schemas-any-com:service:Any:1'                      => []
         // unnötig
         //'urn:dslforum-org:service:WANCommonInterfaceConfig:1'    => ['{FD564AAF-E00A-4CF8-A60D-7F60B4CDFC1B}'=>0], //unnötig!
         //'urn:dslforum-org:service:DeviceConfig:1'                  => [],
